3615d082865bd643abde7de1ed3099060407d4fd
formajasm02 <139533232+formajasm02@users.noreply.github.com>
2023-11-21 08:02:07 -0600

    Update init.js
    Signed-off-by: formajasm02 <139533232+formajasm02@users.noreply.github.com>

--- js/init.js
+++ js/init.js
@@ -1,10 +1,18 @@
-// Configuración de Firebase
-const firebaseConfig = {
-    apiKey: "AIzaSyDSqeyqi7MZRjgl5l11uLMqa_6zbR5VZ6w",
+/*  Inicializa Firebase con la
+ * configuración del proyecto.
+ * Revisa la configuración en tu
+ * servidor de Firebase y cópiala
+ * aquí sustituyendo los
+ * asteriscos. Los campos deben
+ * quedar igual que en tu
+ * servidor. */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyDSqeyqi7MZRjgl5l11uLMqa_6zbR5VZ6w",
   authDomain: "etp43jasm.firebaseapp.com",
   projectId: "etp43jasm",
   storageBucket: "etp43jasm.appspot.com",
   messagingSenderId: "755772794373",
   appId: "1:755772794373:web:54e80da91a8828d3991e6c",
   measurementId: "G-S976KWE18E"
-};
+});
